241ccdb566887e57fb2f38112a1e4e95377fbac6
shawnfisher <sfisher3000@yahoo.com>
2018-08-06 22:41:32 -0700

    Added some UI

--- js/index.js
+++ js/index.js
@@ -14,7 +14,6 @@ function login() {
         } else {
             var provider = new firebase.auth.GoogleAuthProvider();
             firebase.auth().signInWithRedirect(provider);
-            showHome();
         }
     }
     firebase.auth().onAuthStateChanged(newLoginHappened);
@@ -25,36 +24,6 @@ btnLogout.addEventListener('click', e=> {
     firebase.auth().signOut();
 });
 /*End Section -- Firebase Auth*/
-
-// shows sign in page of the app to the user
-function showHome() {
-        document.getElementById('init').click();
-}
-/* End Section -- Initialize*/
-
-/* Start Section -- Navigation for changing tabs */
-function showTab(event, tabName) {
-    // Declare all variables
-    var i, tabContentElements, tabLinkElements;
-
-    // Get all elements with class="tabContent" and hide them
-    tabContentElements = document.getElementsByClassName("tabContent");
-    for (i = 0; i < tabContentElements.length; i++) {
-        tabContentElements[i].style.display = "none";
-    }
-
-    // Get all elements with class="tabLink" and remove the class "active"
-    tabLinkElements = document.getElementsByClassName("tabLink");
-    for (i = 0; i < tabLinkElements.length; i++) {
-        tabLinkElements[i].className =
-            tabLinkElements[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the link
-    document.getElementById(tabName).style.display = "block";
-    event.currentTarget.className += " active";
-}
-/* End Section -- Navigation for changing tabs */
 
 /*Start Section -- Add new item to list*/
 // when a li is clicked in a ul, run this code: Cant get firebase portion to work
