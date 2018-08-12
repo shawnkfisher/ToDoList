26000a5ad1afd8b81700a34c8ed9a41f26431470
shawnfisher <sfisher3000@yahoo.com>
2018-08-12 01:08:48 -0700

    Database is working but list-checkbox-1 needs incremented Cleaned JavaScript

--- js/index.js
+++ js/index.js
@@ -1,67 +1,73 @@
 "use strict";
 
-/*Start Section -- Firebase Auth*/
-// user.displayName
-// user.email
-// user.photoURL
-// user.uid
+//globals
+var taskRef = firebase.database().ref('Task/');
 
+/*Start Section -- Firebase Auth/On Load*/
 function login() {
     function newLoginHappened(user) {
         if (user) {
+            // loads the ToDoList
+            taskRef.on("child_added", function(snapshot) {
+                var task = snapshot.val().Task;
+
+                // Create Elements for ToDoList
+                var li = document.createElement("li");
+                var span = document.createElement("span");
+                var textnode = document.createTextNode(task);
+                var span2 = document.createElement("span");
+                var label = document.createElement("label");
+                var input = document.createElement("input");
+
+                // Classes for elements
+                li.className = 'mdl-list__item';
+                span.className = 'mdl-list__item-primary-content';
+                label.className = 'mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect';
+                label.setAttribute("for", "list-checkbox-1");
+                span2.className = 'mdl-list__item-secondary-action';
+                input.type = 'checkbox';
+                input.id = 'list-checkbox-1';
+                input.className = 'mdl-checkbox__input';
+
+                //Append Elements to DOM
+                document.getElementById("todoList").appendChild(li);
+                span.appendChild(textnode);
+                li.appendChild(span);
+                li.appendChild(span2);
+                span2.appendChild(label);
+                label.appendChild(input);
+                componentHandler.upgradeDom(); //have to upgradeDom when Elements are added Dynamically
+            });
+            // turns off loading screen
             document.getElementById("login-cover").style.display = "none";
         } else {
+            // if user not logged in, redirect to google login
             var provider = new firebase.auth.GoogleAuthProvider();
             firebase.auth().signInWithRedirect(provider);
         }
     }
     firebase.auth().onAuthStateChanged(newLoginHappened);
 }
-
+// Logout button logs out the user
 const btnLogout = document.getElementById('btnLogout');
 btnLogout.addEventListener('click', e=> {
     firebase.auth().signOut();
 });
-/*End Section -- Firebase Auth*/
+/*End Section -- Firebase Auth/ On Load*/
 
-/* Start Section -- add new item to list*/
+/* Start Section -- add new item to ToDolist*/
 function newElement() {
-    var li = document.createElement("li");
-    var span = document.createElement("span");
     var task = document.getElementById("task").value;
-    var textnode = document.createTextNode(task);
-    var firebaseRef = firebase.database().ref('Task/');
 
-    firebaseRef.set(task);
-
-    li.className = 'mdl-list__item';
-    span.className = 'mdl-list__item-primary-content';
-    span.appendChild(textnode);
-
+    // check if input is empty
     if (task === '') {
         alert("You must write something!");
     } else {
-        li.appendChild(span);
-        document.getElementById("todoList").appendChild(li);
+
+        // Push new task to firebase
+        taskRef.push({
+            Task: task
+        });
     }
-    document.getElementById("task").value = "";
-
-    var span2 = document.createElement("span");
-    var label = document.createElement("label");
-    var input = document.createElement("input");
-
-    label.className = 'mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect';
-    label.setAttribute("for", "list-checkbox-2");
-    span2.className = 'mdl-list__item-secondary-action';
-    input.type = 'checkbox';
-    input.id = 'list-checkbox-2';
-    input.className = 'mdl-checkbox__input';
-
-    li.appendChild(span2);
-    span2.appendChild(label);
-    label.appendChild(input);
-    //componentHandler.upgradeElement(label);
-    componentHandler.upgradeDom(); //have to upgradeDom when Elements are added Dynamically
 }
-
 /* End Section -- add new item to list*/
