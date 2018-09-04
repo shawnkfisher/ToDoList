c8aa8106e95f487e43fa9e95932266df9f5857b1
shawnfisher <sfisher3000@yahoo.com>
2018-09-03 22:15:48 -0700

    finishes To-Do List small visual bug with checkbox

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 "use strict";
 
 //globals
-var database = firebase.database(); // database reference
-var auth = firebase.auth(); // authentication reference
+const database = firebase.database(); // database reference
+const auth = firebase.auth(); // authentication reference
+let taskCount = 0; // keeps track of current task
 
 /*Start Section -- Firebase Auth/On Load*/
 function login() {
@@ -11,7 +12,7 @@ function login() {
             loadToDoList(user); //loads toDoList
             document.getElementById("login-cover").style.display = "none";// turns off loading screen
         } else {// if user not logged in, redirect to google login
-            var provider = new firebase.auth.GoogleAuthProvider();
+            const provider = new firebase.auth.GoogleAuthProvider();
             auth.signInWithRedirect(provider);
         }
     }
@@ -21,11 +22,27 @@ function login() {
 function logout() {
     auth.signOut();
 }
+
+function loadToDoList(user) {
+    const taskRef = database.ref('Users/' + user.uid + '/Tasks');
+    const taskCountRef = database.ref('Users/' + user.uid + '/TaskCount');
+    taskCountRef.on("value", function(data) {
+        taskCount = data.val();
+    });
+    taskRef.once('value', function (data) {
+        data.forEach(function(snap) {
+            const task = snap.val().Task; //gets task from firebase
+            makeElement(user.uid, task, snap.key);
+        });
+    });
+}
+
 /*End Section -- Firebase Auth/ On Load*/
 
 function openInput() {
     document.getElementById("mdl-layout__content").style.display = "none";
     document.getElementById("enter-input").style.display = "flex";
+    document.getElementById("task").focus();
 }
 
 document.getElementById('task').addEventListener("keydown", function(e) {
@@ -46,12 +63,11 @@ function newElement() {
     document.getElementById("mdl-layout__content").style.display = "inline-block";
     document.getElementById("enter-input").style.display = "none";
 
-    var task = document.getElementById("task").value;
-    var userId = auth.currentUser.uid;
-    var taskCount = 0;
+    let task = document.getElementById("task").value;
+    const userId = auth.currentUser.uid;
 
     // references the TaskCount stored on firebase for current user
-    var taskCountRef = database.ref('Users/' + userId + '/TaskCount/');
+    const taskCountRef = database.ref('Users/' + userId + '/TaskCount/');
     taskCountRef.transaction(function (current_value) {
         return (current_value || 0) + 1; //increments TaskCount on firebase
     });
@@ -61,102 +77,39 @@ function newElement() {
         taskCount = data.val();
     });
 
+    const setTaskRef = database.ref('Users/' + userId + '/Tasks/' + taskCount);
+
     if (task === '') {
         alert("You must write something!");
     } else { //set task(taskCount) to firebase (triggers on child_added event listener
-        //var taskRef = database.ref('Users/' + userId + '/Tasks/' + '/task' + taskCount);
-        var taskRef = database.ref('Users/' + userId + '/Tasks/' + '/task' + taskCount);
-        taskRef.set({
+        setTaskRef.set({
             Task: task,
-            State: false //keeps track of the state of checkboxes
+            State: false
         });
     }
+
+    const taskRef = database.ref('Users/' + userId + '/Tasks');
+
+    taskRef.on("value", function() {
+        makeElement(userId, task, taskCount);
+        taskRef.off();
+    });
     document.getElementById("task").value = "";
 }
 
 function deleteElement(taskNum) {
-    var userId = auth.currentUser.uid;
-    database.ref('Users/' + userId + '/Tasks/' + '/task' + taskNum).remove();
-    document.getElementById("task" + taskNum).style.display = "none";
-
+    const userId = auth.currentUser.uid;
+    database.ref('Users/' + userId + '/Tasks/' + taskNum).remove();// remove from firebase
+    document.getElementById("task" + taskNum).style.display = "none";// hide on dom
 }
 /* End Section -- add new item to list*/
 
-function loadToDoList(user) {
-    var taskRef = database.ref('Users/' + user.uid + '/Tasks');
-    //var taskCountRef = database.ref('Users/' + user.uid + '/TaskCount/');
-    var taskCount = 0;
-
-    // child_added event listener
-    taskRef.on("child_added", function(data) {
-        taskCount++;
-        var task = data.val().Task; //gets task from firebase
-
-        // Create Elements for ToDoList
-        var li = document.createElement("li");
-        var span = document.createElement("span");
-        var taskNode = document.createTextNode(task);
-        var span2 = document.createElement("span");
-        var label = document.createElement("label");
-        var input = document.createElement("input");
-        var i = document.createElement("i");
-        var icon = document.createTextNode("clear");
-        //var btn = document.createElement("button");
-
-        // Classes for elements
-        li.className = 'mdl-list__item';
-        li.id = 'task'+ taskCount;
-        span.className = 'mdl-list__item-primary-content ';
-        label.className = 'mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect';
-        label.setAttribute("for", "list-checkbox-" + taskCount); // attempting to make checkboxes correspond with elements
-        span2.className = 'mdl-list__item-secondary-action';
-        input.type = 'checkbox';
-        input.id = 'list-checkbox-' + taskCount; // attempting to make checkboxes correspond with elements
-        input.className = 'mdl-checkbox__input';
-        i.className = 'material-icons md-24';
-
-        //Append Elements to DOM
-        document.getElementById("todoList").appendChild(li);
-        li.innerHTML = '<button id="taskBtn' + taskCount +'" onclick="deleteElement('+ taskCount + ')" class="close mdl-button mdl-js-button mdl-js-ripple-effect" />';
-        var btn = document.getElementById('taskBtn' + taskCount);
-        btn.appendChild(i);
-        i.appendChild(icon);
-        span.appendChild(taskNode);
-        li.appendChild(span);
-        li.appendChild(span2);
-        span2.appendChild(label);
-        label.appendChild(input);
-
-        componentHandler.upgradeDom(); //have to upgradeDom when Elements are added Dynamically
-
-        var checkBox = document.getElementById("list-checkbox-" + taskCount);
-        var stateRef = database.ref('Users/' + user.uid + '/Tasks/' + data.key);
-
-        // Get the status on Firebase
-        stateRef.once("value", function(data) {
-            var status = data.child("State").val();
-            //checkBox.setAttribute('checked', status);
-            if (status === true) {
-                label.classList.add("is-checked");
-            }
-        });
-
-        //update value, changed status of checkbox
-        checkBox.addEventListener('change', function() {
-            if(this.checked) {
-                stateRef.update({State: true});
-            } else {
-                stateRef.update({State: false});
-            }
-        });
-    });
-}
-
-var snackbarContainer = document.getElementById('demo-snackbar-example');
-var addBtn = document.getElementById("addBtn");
+/* Start Section -- snackbar*/
+const snackbarContainer = document.getElementById('demo-snackbar-example');
+const addBtn = document.getElementById("addBtn");
 
 addBtn.addEventListener('click', function() {
-    var data = {
+    const data = {
         message: 'Task Added.',
         timeout: 2000,
         actionHandler: handler,
@@ -164,14 +117,72 @@ addBtn.addEventListener('click', function() {
     };
     snackbarContainer.MaterialSnackbar.showSnackbar(data);
 });
-var handler = function(e) {
-    //remove the task
-    // gets TaskCount from firebase
-    var taskCount = 0;
-    var userId = auth.currentUser.uid;
-    var taskCountRef = database.ref('Users/' + userId + '/TaskCount/');
+
+const handler = function() {
+    let taskCount = 0;
+    const userId = auth.currentUser.uid;
+    const taskCountRef = database.ref('Users/' + userId + '/TaskCount/');
     taskCountRef.on("value", function(data) {
         taskCount = data.val();
     });
     deleteElement(taskCount);
 };
+/* End Section -- snackbar*/
+
+function makeElement(userId, task, taskNum) {
+    // Create Elements for ToDoList
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const taskNode = document.createTextNode(task);
+    const span2 = document.createElement("span");
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+    const i = document.createElement("i");
+    const icon = document.createTextNode("clear");
+
+    // Classes for elements
+    li.className = 'mdl-list__item';
+    li.id = 'task'+ taskNum;
+    span.className = 'mdl-list__item-primary-content ';
+    label.className = 'mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect';
+    label.setAttribute("for", "list-checkbox-" + taskNum);
+    span2.className = 'mdl-list__item-secondary-action';
+    input.type = 'checkbox';
+    input.id = 'list-checkbox-' + taskNum;
+    input.className = 'mdl-checkbox__input';
+    i.className = 'material-icons md-24';
+
+    //Append Elements to DOM
+    document.getElementById("todoList").appendChild(li);
+    li.innerHTML = '<button id="taskBtn' + taskNum +'" onclick="deleteElement('+ taskNum + ')" class="close mdl-button mdl-js-button mdl-js-ripple-effect" />';
+    const btn = document.getElementById('taskBtn' + taskNum);
+    btn.appendChild(i);
+    i.appendChild(icon);
+    span.appendChild(taskNode);
+    li.appendChild(span);
+    li.appendChild(span2);
+    span2.appendChild(label);
+    label.appendChild(input);
+
+
+    const stateRef = database.ref('Users/' + userId+ '/Tasks/' + taskNum);
+    const checkBox = document.getElementById("list-checkbox-" + taskNum);
+
+    //update value, changed status of checkbox
+    checkBox.addEventListener('change', function() {
+        if(this.checked) {
+            stateRef.update({State: true});
+        } else {
+            stateRef.update({State: false});
+        }
+    });
+    // gets state of checkbox from firebase
+    stateRef.once("value", function(data) {
+        const state = data.child("State").val();
+        if (state === true) {
+            label.MaterialCheckbox.check();
+        }
+    });
+    componentHandler.upgradeDom(); //have to upgradeDom when Elements are added Dynamically
+
+}
